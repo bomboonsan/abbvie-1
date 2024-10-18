32c35a9690a1592df9db7498cd2a3a025226707b
bomboonsan <bomboonsan@gmail.com>
2024-10-19 00:17:28 +0700

    edit text size

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -2,7 +2,7 @@
 export default function PageTitle({ props }) {
     return (
         <>
-            {props?.text ? <h2 className="text-center text-3xl font-SukhumvitSet font-medium text-[#231f20] mt-10">{props.text}</h2> : null}   
+            {props?.text ? <h2 className="text-center text-2xl md:text-3xl font-SukhumvitSet font-medium text-[#231f20] mt-10">{props.text}</h2> : null}   
         </>
     );
 }
